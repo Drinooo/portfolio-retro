a60cc2b1ed944072765d390233913b85cd02d021
Aldrin Villalobos <alcv@newmediastaff.com>
2023-10-16 10:53:40 +0800

    Major changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -105,7 +105,7 @@ const Contact = () => {
                 id="message"
                 rows="6"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 shadow-sm border-4 border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Leave a comment..."
+                placeholder="Leave a message..."
                 ref={contentRef}
               ></textarea>
             </div>
