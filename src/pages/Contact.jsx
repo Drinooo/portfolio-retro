857b44732a3604ef73794abc5ec76b81b830c4ae
Aldrin Villalobos <alcv@newmediastaff.com>
2023-10-26 10:52:10 +0800

    Minor changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -56,7 +56,7 @@ const Contact = () => {
               <input
                 type="email"
                 id="email"
-                className="shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                className="p-3 shadow-sm bg-gray-50 border-4 border-gray-300 text-gray-900 text-sm focus:ring-primary-500 focus:border-primary-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@gmail.com"
                 ref={emailRef}
                 required
