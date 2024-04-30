ed8cf641151fd0d97aa94d72e2f38a0087a6b835
Aldrin Villalobos <villalobosaldrin18@gmail.com>
2024-04-30 20:56:04 +0800

    Minor changes

--- src/components/Sections/FooterSection.jsx
+++ src/components/Sections/FooterSection.jsx
@@ -60,7 +60,7 @@ const FooterSection = () => {
             </span>
           </div>
           <div className="text-center">
-            <p>This website is inspired by <a href="https://www.mackenziechild.me/" className="text-green">Mackenzie Child's website.</a></p>
+            <p>This website is inspired by <a target="_blank" href="https://www.mackenziechild.me/" className="text-green">Mackenzie Child's website.</a></p>
           </div>
         </div>
       </div>
